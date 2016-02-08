546e282dda0ad355b75064cd4a4f135f7dde0eb7
Scott C. Reynolds <scott@scottcreynolds.com>
2016-02-08 16:22:48 -0500

    solution first draft

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,4 @@
 class SongsController < ApplicationController
-  require 'csv'
 
   def index
     @songs = Song.all
@@ -24,9 +23,7 @@ class SongsController < ApplicationController
   end
 
   def upload
-    CSV.foreach(params["file"].path, headers: true) do |song|
-      Song.create(title: song[0], artist_name: song[1])
-    end
+    SongsWorker.perform_async(params[:file].path)
     redirect_to songs_path
   end
 
